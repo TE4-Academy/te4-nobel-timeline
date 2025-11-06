11e4c5f720f4c5153d63424406dfea99cf7d9491
christianeriksson06 <christianeriksson26@gmail.com>
2025-11-06 14:06:46 +0100

    feature: lägg till Fisher-Yates shuffle

--- data.js
+++ data.js
@@ -3,3 +3,11 @@ export async function loadNobelData() {
     const json = await res.json();
     return json.laureates; // [{id, name, year, category, imageUrl}]
 }
+
+export function shuffle(array) {
+    for (let i =array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
