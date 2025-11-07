087c7a2e6fe189971eb0acfa05ee51efcee4bbc2
christianeriksson06 <christianeriksson26@gmail.com>
2025-11-07 11:55:23 +0100

    feature: spara korrekt ordning baserat på årtal

--- game.js
+++ game.js
@@ -4,3 +4,8 @@ export function countBy(level){
     if(level==='medium') return 8;
     return 12;
 }
+
+export function setPools(laureates) {
+    gameState.pool = laureates;
+    gameState.orderCorrect = [...laureates].sort((a,b) => a.year - b.year).map(x=>x.id);
+}
