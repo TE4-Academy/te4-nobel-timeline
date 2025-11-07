975e80a7793d65bd64fe7995afa84fc12ed8d03c
LiamW11 <liamawiklund06@gmail.com>
2025-11-07 10:13:46 +0100

    feat: aktivera touch DnD när brädet visas closes #4

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ import { loadNobelData } from './data.js';
 import { countBy } from './game.js';
 import { shuffle } from './data.js';
 import { renderBoard } from './ui.js';
+import { wireDnD } from './dnd.js';
 const app = document.getElementById('app');
+const pool = shuffle(all).slice(0, count);
 renderStart(app);
+renderBoard(app, pool);
 
 loadNobelData().then(list => console.log('Antal pristagare:', list.length));
 
