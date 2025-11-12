91567d999a3f30322e388a36a08d2bea1c179b87
LiamW11 <liamawiklund06@gmail.com>
2025-11-12 11:58:59 +0100

    small changes

--- ui.js
+++ ui.js
@@ -1,6 +1,6 @@
 export function renderStart(root) {
   root.innerHTML = `
-  <section class="mx-auto max-w-2xl text-center">
+  <section class="card mx-auto max-w-2xl text-center">
     <h2 class="text-2xl font-bold mb-6">Välj svårighetsgrad</h2>
     <div class="flex flex-col gap-4 items-center">
       <button class="btn btn-primary py-4 text-base w-3/4 sm:w-2/3 md:w-1/2" data-level="easy">Lätt (5)</button>
